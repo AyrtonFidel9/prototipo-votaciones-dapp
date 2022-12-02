8b6e90c4b37920a8c297594887b056ed01fc98ab
AyrtonFidel9 <ayrtonfidel9@gmail.com>
2022-12-02 15:28:09 -0500

    update

--- app/database.js
+++ app/database.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 
 export const sequelize = new Sequelize('vote-coac', 'postgres', 'postgres', {
-    host: '172.31.89.32',
+    host: '172.19.152.30',
     dialect: 'postgres',
 },
 );

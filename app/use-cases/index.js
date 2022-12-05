104878c6d92503b4df7f49cd6ab9bdc06101adef
Edwin Manzano <edwinmanzanoquinzo@gmail.com>
2022-12-04 19:53:48 -0500

    Cambio en el index de la carpeta UseCases

--- app/use-cases/index.js
+++ app/use-cases/index.js
@@ -1,2 +1,4 @@
-export * from './agencias';
-export * from './cuenta';
+export * as Agencias from './agencias/index.js';
+export * as Cuentas from './cuenta/index.js';
+export * as Elecciones from './elecciones/index.js';
+
---
+++ app/use-cases/inscripciones/create.inscripciones.js
@@ -0,0 +1,32 @@
+import * as db from '../../models/index.js';
+import { Inscripciones } from db.Inscripciones
+
+export const create = (req, res) => {
+    // Validate request
+    if (!req.body.formulario) {
+      res.status(400).send({
+        message: "Content can not be empty!"
+      });
+      return;
+    }
+  
+    // Create a inscripcion
+    const inscripcion = {
+      formulario: req.body.formulario,
+      declaracion: req.body.declaracion,
+      estado: req.body.estado,
+      idAgencia: req.body.idAgencia,
+    };
+  
+    // Save inscripcion in the database
+    Inscripciones.create(inscripcion)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the inscripcion."
+        });
+      });
+  };

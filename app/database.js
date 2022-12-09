51ce7b1ce194cced7eb717f337a2af25ec35ced2
Edwin Manzano <edwinmanzanoquinzo@gmail.com>
2022-12-08 20:22:47 -0500

    Configuracion de la base de datos

--- app/database.js
+++ app/database.js
@@ -3,9 +3,9 @@ import Sequelize from 'sequelize';
 export const sequelize = new Sequelize(
     'vote-coac', // nombre de la base de datos 
     'postgres', // usuario
-    'postgres', // contraseña
+    '',//'postgres', // contraseña
     {
-        host: '172.20.249.214', //ip del servidor
+        host: 'localhost',//'172.20.249.214', //ip del servidor
         dialect: 'postgres', //nombre del gestor de base de datos - SQL
     },
 );

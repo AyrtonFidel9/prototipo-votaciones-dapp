2518839265e701061f3f6ae345c99fc12decbaf2
Edwin Manzano <edwinmanzanoquinzo@gmail.com>
2023-01-30 00:04:16 -0500

    Middleware de justificaciones para subir documentos

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -5,3 +5,4 @@ export * from './socios/index.js';
 export * from './verifySignUp.js';
 export * from './representantes/index.js';
 export * from './inscripciones/index.js';
+export * from './justificacion/index.js';
---
+++ app/middleware/justificacion/uploadDocs.js
@@ -0,0 +1,27 @@
+import multer from "multer";
+import path from 'path';
+
+const __dirname = process.cwd();
+
+const pdfTypesFilter = async (req, file, cb) => {
+    if (file.mimetype.includes('application/pdf')) {
+        cb(null, true);
+    } else {
+        req.fileValidationError = 'Solo se aceptan archivos de formato: PDF';
+        cb(null, false, req.fileValidationError);
+    }
+}
+
+const storage = multer.diskStorage({
+    filename: (req, file, cb) => {
+        req.body[file.fieldname] = `${Date.now()}-${file.originalname}`;
+        cb(null, `${Date.now()}-${file.originalname}`, req);
+    },
+    destination: path.join(__dirname, `/app/public/docs/justificacion/`),
+});
+
+export const uploadJustificacion = multer({
+    storage: storage,
+    fileFilter: pdfTypesFilter
+});
+

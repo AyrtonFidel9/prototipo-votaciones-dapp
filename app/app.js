09330579fc8498d4f3789e9273fc107f459ce4cc
AyrtonFidel9 <ayrtonfidel9@gmail.com>
2023-06-06 22:30:07 -0500

    se cambio la ruta de contrato inteligente

--- app/app.js
+++ app/app.js
@@ -1,38 +1,43 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import routes from './routes/index.js';
-import swaggerUi from 'swagger-ui-express';
-import { readFile } from 'fs/promises';
-import YAML from 'js-yaml';
-import { uploadDocs, uploadFile } from './middleware/index.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import routes from "./routes/index.js";
+import swaggerUi from "swagger-ui-express";
+import { readFile } from "fs/promises";
+import YAML from "js-yaml";
+import { uploadDocs, uploadFile } from "./middleware/index.js";
+import path from "path";
+import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-
-const swaggerDocument  = YAML.load(
-    await readFile(new URL('../swagger.yaml', import.meta.url)));
+const swaggerDocument = YAML.load(
+  await readFile(new URL("../swagger.yaml", import.meta.url))
+);
 
 const app = express();
 
 const corsOptions = {
-    origin: "http://localhost:3000"
+  origin: "*",
 };
 
-
 app.use(cors(corsOptions));
-app.use('/images',express.static(path.join(__dirname, '/public/images/')));
-app.use('/files',express.static(path.join(__dirname, '/public/docs/inscripcion')));
-app.use('/justificacion', express.static(path.join(__dirname, '/public/docs/justificacion')));
+app.use("/images", express.static(path.join(__dirname, "/public/images/")));
+app.use(
+  "/files",
+  express.static(path.join(__dirname, "/public/docs/inscripcion"))
+);
+app.use(
+  "/justificacion",
+  express.static(path.join(__dirname, "/public/docs/justificacion"))
+);
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
-app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use("/api/v1", routes);	
+app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api/v1", routes);
 
 export default app;
